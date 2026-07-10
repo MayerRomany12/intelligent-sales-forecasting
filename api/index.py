95c504178c6faddbad44945920fd3c54a597f813
Your Name <you@example.com>
2026-07-10 03:09:37 +0300

    Fix Vercel entrypoint using api/index.py and remove pyproject.toml

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-# Add root directory to the python path so it can import from `src`
+# Add root directory to python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from src.api.app import app
+from src.api.app import app as _app
+app = _app
